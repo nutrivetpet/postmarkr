e79d53219a9b3d4a427b2a6f4b7c1d4d04e30df8
Andrea Ranzato <andrea@nutrivetpet.com>
2025-11-11 17:53:15 +0100

    tests: remove duplicated

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -7,15 +7,6 @@ test_that("get_token() works", {
   expect_true(is_scalar_character(get_token("test")))
 })
 
-test_that("get_token() works", {
-  skip_on_cran()
-  skip_if_not(
-    nzchar(Sys.getenv("POSTMARK_TEST_SERVER_TOKEN")),
-    "Postmark API Key is missing"
-  )
-  expect_true(is_scalar_character(get_token("test")))
-})
-
 test_that("generate_offset_batches() works", {
   expect_identical(
     generate_offset_batches(1500L),
