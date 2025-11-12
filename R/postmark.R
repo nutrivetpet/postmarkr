db2f36d314fcb7a72cb4141a3cddcdcfd21637eb
Andrea Ranzato <andrea@nutrivetpet.com>
2025-11-12 14:55:13 +0100

    print

--- R/postmark.R
+++ R/postmark.R
@@ -109,3 +109,18 @@ postmark <- new_class(
     )
   )
 )
+
+#' @export
+method(print, postmark) <- function(x, ...) {
+  redacted_token <- paste0(
+    substr(x@token, 1, 8),
+    "-****-****-****-************"
+  )
+
+  cat("<postmarkr::postmark>\n")
+  cat("  @ token          : chr ", redacted_token, "\n", sep = "")
+  cat("  @ message_stream : chr ", x@message_stream, "\n", sep = "")
+  cat("  @ base_url       : chr ", x@base_url, "\n", sep = "")
+  cat("  @ timeout        : num ", x@timeout, " seconds\n", sep = "")
+  invisible(x)
+}
