ffdc414774564d26319b5b2e1d556261b28d8c1a
Andrea Ranzato <andrea@nutrivetpet.com>
2025-11-12 19:00:20 +0100

    send

---
+++ R/send.R
@@ -0,0 +1,27 @@
+#' Send
+#' @export
+send <- new_generic("send", c("client", "message"))
+
+#' @export
+method(send, list(postmarkr, email)) <- function(client, message) {
+  req <- build_req_s7(
+    client = client,
+    endpoint = "/email",
+    method = "POST"
+  )
+
+  bdy <- as_api_body(message)
+  bdy$MessageStream <- client@message_stream
+
+  req <- req_body_json(req, bdy)
+
+  resp <- req_perform(req)
+
+  postmarkr_response(
+    data = resp_body_json(resp),
+    status = resp_status(resp),
+    request = req,
+    response = resp,
+    success = isFALSE(resp_is_error(resp))
+  )
+}
