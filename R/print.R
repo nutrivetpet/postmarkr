1003aab9dbd2a71c042eaaea5f5b5a4c0778137c
Andrea Ranzato <andrea@nutrivetpet.com>
2025-11-12 16:26:33 +0100

    print

---
+++ R/print.R
@@ -0,0 +1,14 @@
+#' @export
+method(print, postmarkr) <- function(x, ...) {
+  redacted_token <- paste0(
+    substr(x@token, 1, 8),
+    "-****-****-****-************"
+  )
+
+  cat("<postmarkr::postmark>\n")
+  cat("  @ token          : chr ", redacted_token, "\n", sep = "")
+  cat("  @ message_stream : chr ", x@message_stream, "\n", sep = "")
+  cat("  @ base_url       : chr ", x@base_url, "\n", sep = "")
+  cat("  @ timeout        : num ", x@timeout, " seconds\n", sep = "")
+  invisible(x)
+}
