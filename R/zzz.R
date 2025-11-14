c27071fcf36203f189514c1207ec596806f53680
Andrea Ranzato <andrea@nutrivetpet.com>
2025-11-14 09:59:25 +0100

    remove

--- R/zzz.R
+++ R/zzz.R
@@ -8,51 +8,51 @@ method(as_api_body, Email) <- function(x) {
     To = paste0(x@to, collapse = ", ")
   )
 
-  if (length(x@subject) > 0) {
+  if (length(x@subject)) {
     body$Subject <- x@subject
   }
 
-  if (length(x@cc) > 0) {
+  if (length(x@cc)) {
     body$Cc <- paste0(x@cc, collapse = ", ")
   }
 
-  if (length(x@bcc) > 0) {
+  if (length(x@bcc)) {
     body$Bcc <- paste0(x@bcc, collapse = ", ")
   }
 
-  if (length(x@reply_to) > 0) {
+  if (length(x@reply_to)) {
     body$ReplyTo <- x@reply_to
   }
 
-  if (length(x@tag) > 0) {
+  if (length(x@tag)) {
     body$Tag <- x@tag
   }
 
-  if (length(x@html_body) > 0) {
+  if (length(x@html_body)) {
     body$HtmlBody <- as.character(x@html_body)
-  } else if (length(x@text_body) > 0) {
+  } else if (length(x@text_body)) {
     body$TextBody <- as.character(x@text_body)
   } else {
     pstmrk_abort("Missing body", class = "postmarkr_error_internal")
   }
 
-  if (length(x@track_opens) > 0) {
+  if (length(x@track_opens)) {
     body$TrackOpens <- x@track_opens
   }
 
-  if (length(x@track_links) > 0) {
+  if (length(x@track_links)) {
     body$TrackLinks <- x@track_links
   }
 
-  if (length(x@metadata) > 0) {
+  if (length(x@metadata)) {
     body$Metadata <- x@metadata
   }
 
-  if (length(x@headers) > 0) {
+  if (length(x@headers)) {
     body$Headers <- x@headers
   }
 
-  if (length(x@attachments) > 0) {
+  if (length(x@attachments)) {
     body$Attachments <- x@attachments
   }
 
