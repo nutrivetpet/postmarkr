c5dcf9ced8b64ae33cfa0af0f467003200eb2b66
Andrea Ranzato <andrea@nutrivetpet.com>
2025-11-14 10:53:16 +0100

    Revert "temp redact"
    This reverts commit e7953819f0594f8f72de15e11437038f876ece0e.

--- R/print.R
+++ R/print.R
@@ -1,17 +1,14 @@
 #' @export
 method(print, Postmarkr) <- function(x, ...) {
-  # Temporarily redact the token for printing
-  original_token <- x@token
-  x@token <- paste0(
-    substr(original_token, 1, 8),
+  redacted_token <- paste0(
+    substr(x@token, 1, 8),
     "-****-****-****-************"
   )
 
-  # Use default S7 print method with redacted token
-  print.default(x)
-
-  # Restore original token (just in case, though x is local)
-  x@token <- original_token
-
+  cat("<Postmarkr::postmark>\n")
+  cat("  @ token          : chr ", redacted_token, "\n", sep = "")
+  cat("  @ message_stream : chr ", x@message_stream, "\n", sep = "")
+  cat("  @ base_url       : chr ", x@base_url, "\n", sep = "")
+  cat("  @ timeout        : num ", x@timeout, " seconds\n", sep = "")
   invisible(x)
 }
