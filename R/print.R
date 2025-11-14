2de91cfd850c6ee64ebf0c773fe3c7faf3aadb2c
Andrea Ranzato <andrea@nutrivetpet.com>
2025-11-14 10:52:30 +0100

    temp redact

--- R/print.R
+++ R/print.R
@@ -1,14 +1,17 @@
 #' @export
 method(print, Postmarkr) <- function(x, ...) {
-  redacted_token <- paste0(
-    substr(x@token, 1, 8),
+  # Temporarily redact the token for printing
+  original_token <- x@token
+  x@token <- paste0(
+    substr(original_token, 1, 8),
     "-****-****-****-************"
   )
 
-  cat("<Postmarkr::postmark>\n")
-  cat("  @ token          : chr ", redacted_token, "\n", sep = "")
-  cat("  @ message_stream : chr ", x@message_stream, "\n", sep = "")
-  cat("  @ base_url       : chr ", x@base_url, "\n", sep = "")
-  cat("  @ timeout        : num ", x@timeout, " seconds\n", sep = "")
+  # Use default S7 print method with redacted token
+  print.default(x)
+
+  # Restore original token (just in case, though x is local)
+  x@token <- original_token
+
   invisible(x)
 }
