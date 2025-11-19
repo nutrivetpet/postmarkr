c4e27e21d84a762d8d2196b30218356117b27ac3
Andrea Ranzato <andrea@nutrivetpet.com>
2025-11-19 11:40:24 +0100

    clean up and get token

--- R/utils.R
+++ R/utils.R
@@ -1,42 +1,3 @@
-get_token <- function(env = c("live", "test")) {
-  env <- arg_match(env, env)
-  envar <- switch(
-    env,
-    test = "POSTMARK_TEST_SERVER_TOKEN",
-    live = "POSTMARK_SERVER_TOKEN"
-  )
-  token <- Sys.getenv(envar)
-  if (!nzchar(token)) {
-    abort(
-      c(
-        "Cannot find token for API authentication.",
-        i = sprintf(
-          "Did you forget to set the `%s` env. variable?",
-          envar
-        )
-      ),
-      class = "postmarkr_error_missing_token"
-    )
-  }
-  token
-}
-
-build_header <- function(req, token) {
-  req_headers(
-    req,
-    "Accept" = "application/json",
-    "X-Postmark-Server-Token" = token,
-    .redact = "X-Postmark-Server-Token"
-  )
-}
-
-generate_offset_batches <- function(n) {
-  stopifnot("`n` must be a single integer" = is_integer(n, 1L))
-  offset <- seq(0L, n, by = 500L)
-  count <- rep(500L, length(offset))
-  data.frame(count = count, offset = offset)
-}
-
 supported_args <- function() {
   c(
     "count",
@@ -52,64 +13,6 @@ supported_args <- function() {
     "metadata_",
     "type"
   )
-}
-
-capitalize_first <- function(x) {
-  stopifnot(
-    "`x` must be a character vector" = is_character(x)
-  )
-
-  vec <- strsplit(x, split = "")
-  uppercase <- lapply(vec, function(x) {
-    if (length(x) == 0L) "" else toupper(x[[1L]])
-  })
-
-  out <- Map(
-    function(x, y) {
-      replace(x, 1L, y)
-    },
-    vec,
-    uppercase,
-    USE.NAMES = FALSE
-  )
-
-  unlist(lapply(out, function(x) paste0(x, collapse = "")))
-}
-
-split_vec <- function(x, n) {
-  stopifnot(
-    # "`x` must be a character vector" = is_character(x),
-    "`n` must be a single integer" = is_scalar_integer(n),
-    "`n` must be greater than 0" = n > 0L,
-    "`x` must not be empty" = length(x) > 0L
-  )
-
-  groups <- ceiling(seq_along(x) / n)
-
-  unname(split(x, groups))
-}
-
-rep_list <- function(x, n) {
-  stopifnot(
-    "`x` must be a list" = is_list(x),
-    "`x` must be a named list" = is_named(x)
-  )
-
-  template_fn <- function(...) {
-    args <- list(...)
-    args <- set_names(x, names(x))
-    args
-  }
-
-  reps <- lapply(x, function(x) rep(x, n))
-
-  args <- c(
-    list(f = template_fn),
-    reps,
-    list(USE.NAMES = FALSE)
-  )
-
-  exec("Map", !!!args)
 }
 
 collapse_comma <- function(x) {
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,38 +1,3 @@
-test_that("get_token() works", {
-  skip_on_cran()
-  skip_if_not(
-    nzchar(Sys.getenv("POSTMARK_TEST_SERVER_TOKEN")),
-    "Postmark API Key is missing"
-  )
-  expect_true(is_scalar_character(get_token("test")))
-})
-
-test_that("generate_offset_batches() works", {
-  expect_identical(
-    generate_offset_batches(1500L),
-    data.frame(count = 500L, offset = seq(0L, 1500L, by = 500L))
-  )
-})
-
-test_that("capitalize_first() works", {
-  expect_identical(capitalize_first("test"), "Test")
-  expect_identical(
-    capitalize_first(c("test01", "test02")),
-    c("Test01", "Test02")
-  )
-  expect_identical(
-    capitalize_first(c("test01", "")),
-    c("Test01", "")
-  )
-})
-
-test_that("rep_list() works", {
-  expect_identical(
-    rep_list(list(a = "a", b = "b"), 2L),
-    list(list(a = "a", b = "b"), list(a = "a", b = "b"))
-  )
-})
-
 test_that("extract_email() extracts email from name-formatted strings", {
   expect_identical(
     extract_email("John Doe <john@example.com>"),
@@ -54,7 +19,10 @@ test_that("validate_email() accepts valid email formats", {
   expect_invisible(expect_null(validate_email("user+tag@example.co.uk")))
   expect_invisible(expect_null(validate_email("user_name@sub.example.com")))
   expect_invisible(expect_null(validate_email("John Doe <john@example.com>")))
-  expect_invisible(expect_null(validate_email(c("alice@example.com", "bob@example.org"))))
+  expect_invisible(expect_null(validate_email(c(
+    "alice@example.com",
+    "bob@example.org"
+  ))))
 })
 
 test_that("validate_email() rejects invalid email formats", {
