4c67b0ed32fce729065e860f37c5fe3003f1f269
Andrea Ranzato <andrea@nutrivetpet.com>
2025-11-19 11:22:22 +0100

    remove

--- R/build_req.R
+++ R/build_req.R
@@ -1,29 +1,3 @@
-build_req <- function(endpoint, method, env, ...) {
-  dots <- list2(...)
-  token <- get_token(env)
-
-  req <-
-    request(POSTMARK_BASE_URL) |>
-    req_url_path_append(endpoint) |>
-    req_method(method) |>
-    build_header(token)
-
-  if (length(dots)) {
-    args <- names2(dots)
-
-    if (!all(args %in% supported_args())) {
-      abort(
-        "Some arguments are not supported.",
-        class = "postmarkr_error_not_supported_args"
-      )
-    }
-
-    req <- req_url_query(req, !!!dots)
-  }
-
-  req
-}
-
 #' Build HTTP Request for S7 Client
 #'
 #' Creates an httr2 request object using a postmarkr S7 client.
